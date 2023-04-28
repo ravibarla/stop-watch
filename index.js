5c8207db4931a4e4455ef2b6a8c3fd3437054ca6
ravi barla <ravibarla1@gmail.com>
2023-04-28 20:25:55 +0530

    adding css and updating the comments

--- index.js
+++ index.js
@@ -14,32 +14,59 @@ function showNotification(txt) {
     clearInterval(myInterval)
   }
   
+ //reset-color buttons 
+function reset(id){
+    document.getElementById(id).style.backgroundColor=`rgb(164, 8, 248)`
+}  
 
-function clickHandler(e) {
-    //function when start is clicked
-    if(e.target.name=='start'){
-        intialiseStopWatchApp() 
+//color-changing button handler
+function changeBtnColor(id){
+    if(id=="start"){
+        reset('stop')
+        reset('reset')
+        document.getElementById(id).style.backgroundColor=`rgb(250, 21, 215)`
+        
     }
-    //function when stop is clicked
+    if(id=="stop"){
+        reset('start')
+        reset('reset')
+        document.getElementById(id).style.backgroundColor=`rgb(250, 21, 215)`
+    }
+    if(id=="reset"){
+        reset('start')
+        reset('stop')
+        
+    }     
+}
+
+//click handler
+function clickHandler(e) {
+    if(e.target.name=='start'){
+        //start button handler
+        intialiseStopWatchApp() 
+        changeBtnColor(e.target.id)
+    }
+    //stop button handler
     if(e.target.name=='stop'){
       clearInterval(myInterval)
-      
+      changeBtnColor(e.target.id)
     }
-    //function when reset is clicked
+    //reset button handler
     if(e.target.name=='reset'){
         clearInterval(myInterval)
         Timer.minute=0
         Timer.seconds=0
         timeElement.innerHTML="00:00"
+        changeBtnColor(e.target.id)
     }
 }
 
-//format the time into 2-Digits
+// 2-digit Time formatter
 function formatTime(time) {
     return (time<10 ||time==0)?'0'+time:time
 }
 
-//callback function to update the timer when timer is started
+//callback function to update the timer
 function updateCurrentTime() {
     Timer.seconds++
     if(Timer.seconds==Timer.maxSeconds){
@@ -49,10 +76,11 @@ function updateCurrentTime() {
     timeElement.innerHTML=formatTime(Timer.minute)+' : '+formatTime(Timer.seconds)
 }
 
-//function to initialise the timer
+//initialise the timer
 function intialiseStopWatchApp() {
     myInterval=setInterval(updateCurrentTime,1000)
 }
 
-//tracking the click actions
+//tracking click actions
 document.addEventListener("click",clickHandler)
+
